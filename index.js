c78140ece4b8aaf27375ad00f87d8a997272be7e
ViniiR <cvrm2004@gmail.com>
2023-08-24 21:03:01 -0300

    menu css, js and website body done

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 "use strict";
 
-const menuButton = document.querySelector('.menuButton');
+const menuButton = document.querySelector(".menuButton");
+const input = menuButton.querySelector("input");
 
+input.addEventListener("click", function () {
+    const sideBar = document.querySelector(".sideBar");
+    if (
+        sideBar.style.transform === "" ||
+        sideBar.style.transform === "translateX(-100%)"
+    ) {
+        sideBar.style.transform = "translateX(0%)";
+    } else {
+        sideBar.style.transform = "translateX(-100%)";
+    }
+    console.log(sideBar.style.transform);
+});
