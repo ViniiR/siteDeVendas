59f88d7c8adea33391d3da3c00de542c8c24ea73
ViniiR <cvrm2004@gmail.com>
2023-08-26 19:52:18 -0300

    giving up on the project

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
 "use strict";
 
-const menuButton = document.querySelector(".menuButton");
-const input = menuButton.querySelector("input");
-
-input.addEventListener("click", function () {
-    const sideBar = document.querySelector(".sideBar");
-    if (
-        sideBar.style.transform === "" ||
-        sideBar.style.transform === "translateX(-100%)"
-    ) {
-        this.style.position = "fixed";
-    } else {
-        this.style.position = "";
-    }
-    console.log(this.style.position);
-});
